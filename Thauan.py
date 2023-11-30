535376c0b050b87a142236541f4c1e5d7b40914b
ThFoxs2 <152522821+ThFoxs2@users.noreply.github.com>
2023-11-30 14:14:44 -0300

    Add files via upload

---
+++ Thauan.py
@@ -0,0 +1,36 @@
+
+
+'''num1 = st.number_input('Digite o primeiro número:')
+num2 = st.number_input('Digite o outro número: ')
+
+if st.button('somar'):
+ res = num1 + num2
+ st.text("resultado: ")
+ st.title(res)
+
+if st.button('subtrair'):
+  sub = num1 - num2
+  st.text("resultado: ")
+  st.title(sub)
+
+if st.button('multiplicar'):
+   mult = num1 * num2
+   st.text("resultado: ")
+   st.title(mult)
+
+if st.button('dividir'):
+  div = num1 / num2
+  st.text("resultado: ")
+  st.title(div)'''
+
+'''num1 = st.number_input("Digite a temperatura atual em Grau Celsius: ")
+
+
+if st.button("Grau Celsius em Grau Fahrenheit"):
+    resultado = (num1*1.8) + 32
+    st.text("Resultado")
+    st.title(resultado)'''
+
+import streamlit as st
+
+st.title('Seja Bem vindo ')
